da15e42f3807dd321d4a376715cedd81fd620972
Faruq-Hameed <faruqhameed1@gmail.com>
2024-10-20 07:52:47 +0100

    Initial commit
    Generated by create-expo-app 3.0.0.

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {Colors} from "./constants/Colors"
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
