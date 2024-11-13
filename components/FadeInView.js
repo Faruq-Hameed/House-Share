f36e3de89e263de97d324f6aaf7da082966089f5
Faruq-Hameed <faruqhameed1@gmail.com>
2024-11-13 17:03:43 +0100

    converted to class component

--- components/FadeInView.js
+++ components/FadeInView.js
@@ -1,28 +1,22 @@
 //Animation component
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Animated, Text, View } from "react-native";
 
-export class FadeInView extends React.PureComponent {
-  state = {
-    fadeIn: new Animated.Value(0),
-  };
-  componentDidMount() {
-    Animated.timing(this.state.fadeIn, {
+export const FadeInView = (props) => {
+  const [fadeIn] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(fadeIn, {
       toValue: 1,
       duration: 4000,
       useNativeDriver: true,
     }).start();
-  }
-  render() {
+  }, [fadeIn]); // Empty dependency array ensures this effect runs once after initial render)
 
-    return (
-      <Animated.View
-        style={{ ...this.props.style, opacity: this.state.fadeIn }}
-      >
-        {this.props.children}
-      </Animated.View>
-    );
-  }
-}
-
+  return (
+    <Animated.View style={{ ...props.style, opacity: fadeIn }}>
+      {props.children}
+    </Animated.View>
+  );
+};
