b3af2d46fd9366663923ae5d359ad42f67a62dff
Faruq-Hameed <faruqhameed1@gmail.com>
2024-11-21 17:07:32 +0100

    code update

--- components/FadeInView.js
+++ components/FadeInView.js
@@ -6,13 +6,6 @@ import { Animated, Text, View } from "react-native";
 export const FadeInView = (props) => {
   const [fadeIn] = useState(new Animated.Value(0));
 
-  useEffect(() => {
-    Animated.timing(fadeIn, {
-      toValue: 1,
-      duration: 4000,
-      useNativeDriver: true,
-    }).start();
-  }, [fadeIn]); // Empty dependency array ensures this effect runs once after initial render)
 
   return (
     <Animated.View style={{ ...props.style, opacity: fadeIn }}>
